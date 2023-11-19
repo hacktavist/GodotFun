4264813ca7923632ff188015cb5cd60add718e7f
NP <hacktavist616@gmail.com>
2023-11-19 00:59:25 -0500

    enabled particle effect in script

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,5 +1,10 @@
 extends ColorRect
 
+@onready var cpu_particles_2d = $CPUParticles2D
+
+func _ready():
+	cpu_particles_2d.emitting = true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
