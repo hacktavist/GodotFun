3b99a8f6d4d185cfee0f5b6fb002986bb5ea6b62
NP <hacktavist616@gmail.com>
2023-11-21 20:48:47 -0500

    after refactoring of storing levels in Gamemanager.gd updated exit_trigger.gd to work with that flow

--- exit_trigger.gd
+++ exit_trigger.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
-func _ready():
-	print("Exit")
 
+################################################################################
+# TODO: setup logical functions to switch player between random scenes based
+#       on where they are in the game right now ie lvl1,lvl2,lvl3 etc
+################################################################################
 func _on_body_entered(body):
-	print("Triggered!!!")
+	GameManager.is_player_input_disabled = true
+	get_tree().change_scene_to_packed(GameManager.random_intro_level())
